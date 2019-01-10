09664bac0a7f8909784bcc18278ce42d4c0e2666
turmakhanovD <46023441+turmakhanovD@users.noreply.github.com>
2019-01-10 15:08:59 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your FIO: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+
+            Console.Write("Enter 2 nums: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", firstNum + secondNum);
+        }
+    }
+}
